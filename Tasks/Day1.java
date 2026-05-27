631587d2b37644be54dc9c9b5042da81d40dc73b
Cherukumudi Bramhendra <bramicherukumudinaidu@gmail.com>
2026-05-27 10:52:15 +0530

    Update Day1.java

--- Tasks/Day1.java
+++ Tasks/Day1.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 class Day1{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
+		
+		/*Q1 . Electricty bill calculation : 
+				--> if(units <= 250) bill is 0 
+				else bill is substarct by 250 like units - 250
+		*/ 
 		System.out.print("Eneter ele uints : ");
 		int units = sc.nextInt();
 		System.out.println("Electricty bill " + eleBill(units));
 		System.out.println();
 		
+		//Q2 . Using Loops print the pattren Pyramid
 		System.out.print("Eneter pyramid size : ");
 		int n = sc.nextInt();
 		System.out.println();
 		PyramidPat(n);
 		System.out.println();
 		
+		//Q3 . Using loops print the left angle triangle
 		System.out.print("Eneter left angle triablge size : ");
 		n = sc.nextInt();
 		System.out.println();

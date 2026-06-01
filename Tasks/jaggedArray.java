a1c4fc609b0aba109813bcfc39733652df5fce0b
Bramhendra-C <bramicherukumudinaidu@gmail.com>
2026-06-01 14:48:17 +0530

    added recursion

---
+++ Tasks/jaggedArray.java
@@ -0,0 +1,29 @@
+package CODE.Tasks;
+
+public class jaggedArray {
+
+	public static void main(String[] args) {
+		int[][] marks = new int[3][0];
+		marks[0] = new int[4];
+		marks[1] = new int[7];
+		marks[2] = new int[5];
+		
+		for(int i=0;i<marks.length;i++) {
+			for(int j=0;j<marks[i].length;j++) {
+				marks[i][j] = (i - marks.length) * (j - marks.length);
+			}
+		}
+		
+		print(marks);
+		
+	}
+	static void print(int[][] marks) {
+		for(int[] row : marks) {
+			for(int ele : row) {
+				System.out.print(ele + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
